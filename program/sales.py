89d15bda19fd025c101fc588e1dd8f90bc71e7b6
sawyermade <sawyermade0@gmail.com>
2022-04-07 00:55:28 -0400

    first

--- program/sales.py
+++ program/sales.py
@@ -54,8 +54,7 @@ def save_data(data_dict, data_path):
 	# Saves updated data dict to csv file we opened
 	with open(data_path, 'w') as cf:
 		writer = csv.writer(cf)
-		for item in data_dict.items():
-			writer.writerows([item])
+		writer.writerows(list(data_dict.items()))
 
 def main():
 	# CSV data path, works both in and just outside program directory.
